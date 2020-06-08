ad3ee57c856eef00c584c23c4c597d161632d5d9
Luann Sapucaia <luannsapucaia@gmail.com>
2020-06-08 16:42:59 -0300

    Updates

---
+++ precache-manifest.c53eaa540b253817569f7dd5efb3f93d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1caa7e90a0bd8f3c416a678b876e1d78",
+    "url": "/medium-chart/index.html"
+  },
+  {
+    "revision": "e8d666b62e03a3743fde",
+    "url": "/medium-chart/static/css/main.2ea99353.chunk.css"
+  },
+  {
+    "revision": "e2b8b04a5759a158bb15",
+    "url": "/medium-chart/static/js/2.ccfeedea.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/medium-chart/static/js/2.ccfeedea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8d666b62e03a3743fde",
+    "url": "/medium-chart/static/js/main.17952c16.chunk.js"
+  },
+  {
+    "revision": "cf8571857af123149569",
+    "url": "/medium-chart/static/js/runtime-main.c201d308.js"
+  }
+]);
--- src/api/Api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-
-const api = axios.create({
-    baseURL: 'https://api.covid19api.com',
-})
-
-export default api;
